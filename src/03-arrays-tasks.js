639c4789eda25275f59cd2af1259803286ee7c4e
valerydluski <valerydluski@gmail.com>
2020-05-04 14:26:20 +0300

    feat: add solution for some tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -576,8 +576,8 @@ function selectMany(arr, childrenSelector) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  return indexes.reduce((accumulator, currentValue) => accumulator[currentValue], arr);
 }
 
 
@@ -599,8 +599,16 @@ function getElementByIndexes(/* arr, indexes */) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  const { length } = arr;
+  const head = arr.slice(0, Math.floor(length / 2));
+  const tail = arr.slice(Math.ceil(length / 2), length);
+  if (length % 2 === 0) {
+    return tail.concat(head);
+  }
+  const middle = arr[Math.floor(length / 2)];
+  tail.push(middle);
+  return tail.concat(head);
 }
 
 
