7949496fa4fc8feee39312408e38802db20c4ad9
valerydluski <valerydluski@gmail.com>
2020-05-09 10:44:39 +0300

    feat: add solutions for some tasks

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -295,8 +295,13 @@ function isCreditCardNumber(/* ccn */) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  const reducer = (accumulator, currentValue) => +accumulator + +currentValue;
+  const amount = num.toString().split('').reduce(reducer);
+  if (amount < 9) {
+    return amount;
+  }
+  return getDigitalRoot(amount);
 }
 
 
@@ -346,8 +351,8 @@ function isBracketsBalanced(/* str */) {
  *    365, 4  => '11231'
  *    365, 10 => '365'
  */
-function toNaryString(/* num, n */) {
-  throw new Error('Not implemented');
+function toNaryString(num, n) {
+  return num.toString(n);
 }
 
 
